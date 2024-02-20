10cd9467083ef4b8eb8c9a4faa2d686fef51bbb8
Vladyslav Havriutkin <havriutkin@gmail.com>
2024-02-20 06:07:25 -0500

    Init Spring project

---
+++ src/main/java/com/shinobicoders/teamcodeapi/TeamCodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.shinobicoders.teamcodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCodeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamCodeApiApplication.class, args);
+    }
+
+}
