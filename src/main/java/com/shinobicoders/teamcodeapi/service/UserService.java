1c9b24b8bb56a579cbb526db5cbfc82dbe0a2ac1
Vladyslav Havriutkin <havriutkin@gmail.com>
2024-02-27 12:26:12 -0500

    Implement UserService

--- src/main/java/com/shinobicoders/teamcodeapi/service/UserService.java
+++ src/main/java/com/shinobicoders/teamcodeapi/service/UserService.java
@@ -3,6 +3,8 @@ package com.shinobicoders.teamcodeapi.service;
 import com.shinobicoders.teamcodeapi.model.User;
 import com.shinobicoders.teamcodeapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,5 +27,24 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    public User updateUser(Long id, User userDetails) {
+        // TODO: create custom exception for ResourceNotFound
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
 
+        // todo: can abstract this logic?
+        user.setId(userDetails.getId());
+        user.setName(userDetails.getName());
+        user.setEmail(userDetails.getEmail());
+        user.setPassword(userDetails.getPassword());
+        user.setBio(userDetails.getBio());
+        user.setGithubLink(userDetails.getGithubLink());
+        user.setExperience(userDetails.getExperience());
+
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
