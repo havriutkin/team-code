95ad3c366fa2b1f2a326b391519a23ee7eb5a1a9
Vladyslav Havriutkin <havriutkin@gmail.com>
2024-04-03 18:17:19 -0400

    Add not found exception

--- src/main/java/com/shinobicoders/teamcodeapi/controller/UserController.java
+++ src/main/java/com/shinobicoders/teamcodeapi/controller/UserController.java
@@ -4,6 +4,7 @@ import com.shinobicoders.teamcodeapi.model.User;
 import com.shinobicoders.teamcodeapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,7 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    public ResponseEntity<User> getUserById(@PathVariable Long id) throws ChangeSetPersister.NotFoundException {
         User user = userService.getUserById(id);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
