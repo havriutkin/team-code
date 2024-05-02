104c9bdf5d842743a20fd381aa22a9b2e163e23a
Alex Pliekhov <aleksejplehov441@gmail.com>
2024-05-02 23:40:10 +0300

    add get user principal method

--- src/main/java/com/shinobicoders/teamcodeapi/controller/AuthController.java
+++ src/main/java/com/shinobicoders/teamcodeapi/controller/AuthController.java
@@ -3,20 +3,23 @@ package com.shinobicoders.teamcodeapi.controller;
 import com.shinobicoders.teamcodeapi.auth.LoginRequest;
 import com.shinobicoders.teamcodeapi.auth.LoginResponse;
 import com.shinobicoders.teamcodeapi.auth.RegisterRequest;
+import com.shinobicoders.teamcodeapi.auth.UserPrincipal;
 import com.shinobicoders.teamcodeapi.model.User;
 import com.shinobicoders.teamcodeapi.service.AuthService;
+import com.shinobicoders.teamcodeapi.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.context.SecurityContextHolder;
+import com.shinobicoders.teamcodeapi.auth.UserPrincipal;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final UserService userService;
 
     @PostMapping("/login")
     public LoginResponse login(@RequestBody @Validated LoginRequest request) {
@@ -31,5 +34,11 @@ public class AuthController {
         user.setPassword(request.getPassword());
 
         return authService.register(user);
+    }
+
+    @GetMapping("/user")
+    public UserPrincipal getUser() {
+        UserPrincipal principal = (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return principal;
     }
 }
